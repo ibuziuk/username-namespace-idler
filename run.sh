b9ef1ebbeee2d456b45afa0d2cadb60b72cca963
Ilya Buziuk <ibuziuk@redhat.com>
2020-03-06 18:40:58 +0100

    Initial implementation of the idler script
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ run.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# This script is used for idling resources in the user namespace on Eclipse Che hosted by Red Hat
+#
+# --------------
+# Print Che logo
+# --------------
+
+echo
+cat <<EOF
+[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;52m1[38;5;94m0[38;5;136m1[38;5;215m0[38;5;215m0[38;5;136m0[38;5;94m0[38;5;58m0[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m
+[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;58m0[38;5;136m1[38;5;179m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;179m1[38;5;136m0[38;5;58m1[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m
+[0m [0m [0m [0m [0m [0m [0m [38;5;52m0[38;5;94m1[38;5;136m0[38;5;179m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;179m0[38;5;136m1[38;5;94m1[38;5;52m0[0m [0m [0m [0m [0m [0m [0m
+[0m [0m [0m [38;5;58m1[38;5;136m1[38;5;179m0[38;5;215m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;215m0[38;5;179m0[38;5;100m0[38;5;58m1[0m [0m [0m
+[38;5;136m0[38;5;179m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;136m0[38;5;52m1
+[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;215m0[38;5;179m0[38;5;179m0[38;5;215m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;215m1[38;5;179m1[38;5;100m1[38;5;58m0[0m [0m [0m
+[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;221m0[38;5;179m1[38;5;136m0[38;5;94m0[38;5;52m1[0m [0m [0m [0m [38;5;52m1[38;5;94m0[38;5;136m0[38;5;179m1[38;5;221m1[38;5;221m1[38;5;221m0[38;5;179m1[38;5;136m0[38;5;94m0[38;5;52m0[0m [0m [0m [0m [0m [0m [0m
+[38;5;221m1[38;5;221m0[38;5;221m1[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;215m1[38;5;179m0[38;5;136m0[38;5;58m0[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;58m0[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m
+[38;5;221m1[38;5;221m0[38;5;221m0[38;5;221m1[38;5;221m0[38;5;179m1[38;5;136m0[38;5;94m1[38;5;52m1[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;17m0[38;5;59m1[38;5;60m1[38;5;60m0
+[38;5;221m1[38;5;179m0[38;5;180m1[38;5;138m0[38;5;102m0[38;5;60m0[38;5;23m0[38;5;17m1[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;17m1[38;5;60m1[38;5;60m0[38;5;67m1[38;5;103m1[38;5;103m1[38;5;103m1[38;5;67m1
+[38;5;103m1[38;5;67m1[38;5;61m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;103m1[38;5;103m1[38;5;67m1[38;5;60m0[38;5;60m1[38;5;23m1[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;23m1[38;5;60m1[38;5;60m1[38;5;67m0[38;5;103m1[38;5;103m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m0[38;5;67m0[38;5;67m1
+[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m0[38;5;103m0[38;5;103m1[38;5;103m1[38;5;67m0[38;5;60m1[38;5;59m0[38;5;17m1[0m [0m [0m [0m [0m [0m [38;5;17m0[38;5;59m0[38;5;60m0[38;5;67m0[38;5;67m0[38;5;103m1[38;5;103m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1
+[38;5;103m0[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;103m0[38;5;103m0[38;5;67m0[38;5;60m0[38;5;60m0[38;5;60m0[38;5;60m1[38;5;67m0[38;5;103m1[38;5;103m1[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;103m1
+[38;5;59m1[38;5;60m1[38;5;67m0[38;5;67m1[38;5;103m0[38;5;103m0[38;5;67m0[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m0[38;5;67m0[38;5;103m0[38;5;103m1[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;103m0[38;5;103m1[38;5;67m1[38;5;67m0[38;5;60m1[38;5;59m1
+[0m [0m [0m [0m [38;5;23m0[38;5;60m0[38;5;60m0[38;5;67m0[38;5;103m0[38;5;103m0[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m0[38;5;67m0[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m1[38;5;103m1[38;5;103m0[38;5;67m0[38;5;60m0[38;5;60m0[38;5;23m0[0m [0m [0m [0m
+[0m [0m [0m [0m [0m [0m [0m [0m [38;5;17m0[38;5;59m0[38;5;60m0[38;5;67m1[38;5;103m1[38;5;103m0[38;5;103m1[38;5;67m0[38;5;67m1[38;5;67m1[38;5;67m0[38;5;67m1[38;5;67m0[38;5;67m0[38;5;67m0[38;5;67m1[38;5;67m0[38;5;103m0[38;5;103m0[38;5;103m1[38;5;67m1[38;5;60m1[38;5;60m1[38;5;17m0[0m [0m [0m [0m [0m [0m [0m [0m
+[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;17m1[38;5;59m1[38;5;60m0[38;5;60m1[38;5;67m0[38;5;103m1[38;5;103m1[38;5;67m1[38;5;67m0[38;5;103m0[38;5;103m0[38;5;67m0[38;5;60m1[38;5;60m0[38;5;59m0[38;5;17m1[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m
+[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [38;5;17m0[38;5;60m0[38;5;60m0[38;5;60m1[38;5;60m0[38;5;17m1[0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m [0m
+[0m
+EOF
+echo
+
+HELP="
+--help - script help menu
+--dry-run - a practice run of the script for the testing process where the effects of a possible failure are intentionally mitigated (idling is not going to happen if the option is used).
+"
+
+printInfo() {
+  green=`tput setaf 2`
+  reset=`tput sgr0`
+  echo -e "${green}[INFO]: ${1} ${reset}"
+}
+
+printWarning() {
+  yellow=`tput setaf 3`
+  reset=`tput sgr0`
+  echo -e "${yellow}[WARNING]: ${1} ${reset}"
+}
+
+printError() {
+  red=`tput setaf 1`
+  reset=`tput sgr0`
+  echo -e "${red}[ERROR]: ${1} ${reset}"
+}
+
+for key in "$@"
+do
+case $key in
+    --dry-run)
+    DRY_RUN=true
+    shift
+    ;;
+    --help)
+        printInfo "$HELP"
+        exit 1
+    ;;
+    *)
+    printError "Wrong arg '$key'."
+    printInfo "$HELP"
+    exit 1
+    ;;
+esac
+done
+
+DEFAULT_DRY_RUN="false"
+export DRY_RUN=${DRY_RUN:-${DEFAULT_DRY_RUN}}
+
+# ------------------------------------
+# Get the list of namesaces with pods 
+# ------------------------------------
+getNamespacesWithPods() {
+  NAMESPACES=$(oc get pod --all-namespaces -o jsonpath='{..metadata.namespace}' | xargs -n1 | sort -u)
+  export NAMESPACES
+}
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Idles all the resources in the user namespaces on the cluster via `oc idle --all`.
+# Namespace is treated as a user namespace when a corresponding *-che namespace exists e.g. 'ibuziuk' and 'ibuziuk-che'
+# -----------------------------------------------------------------------------------------------------------------------
+runIdler() {
+  printInfo "Starting the idler for 'user' namespaces on Hosted Che..."
+
+  if [ "${DRY_RUN}" == "true" ]; then
+    printWarning "'--dry-run' option is used. Idling of the resources in the users namespaces is not going to happen during this execution"
+  fi
+
+  getNamespacesWithPods
+  printInfo "The list of namespaces to check on the cluster:\n$NAMESPACES"
+
+  for namespace in $NAMESPACES; do
+      # 1. Let's skip namespaces like 'openshift-infra', 'kube-system', 'ops-health-monitoring', 'k8s-image-puller', 'default' etc.
+      if [[ $namespace == "openshift-"* || $namespace == "kube-"* || $namespace == "ops-"* || $namespace == "sre-"*  || $namespace == "default" || $namespace == "logging" || $namespace == "gluster-volrecycler" || $namespace = "k8s-image-puller" ]]; then
+        printInfo "Skipping '$namespace' system namespace"
+      # 2. Let's skip *-che namespaces (this check can be improved since it does not cover cases when usernames ends with *-che, but let's treat it as an edge-case for now)
+      elif [[ $namespace == *"-che" ]]; then
+        printInfo "Skipping '$namespace' that is used for workspace pods only"
+      else
+        # 3. Let's check if the namespace is a user namespace provisioned for Hosted Che (there should be a corresponding '$namespace-che' for it)
+        printInfo "Checking if '$namespace' namespace has the corresponding '*-che' namespace"
+        oc get project ${namespace}-che 2>&1 > /dev/null
+        if [ $? -eq 0 ]; then
+          # 4. Let's check if there is a running workspaces in the '$namespace-che'
+          printInfo "'$namespace-che' has been found. Checking a workspace pod presense"
+          pods=$(oc get pod --selector che.workspace_id 2>&1 > /dev/null)
+          if [[ $pods == "No resources found." ]]; then
+            # 5. Let's idle all the resources in the ${namespace}            
+            if [ "${DRY_RUN}" == "true" ]; then
+              printWarning "'--dry-run' option is used. Idling of the resources in the '$namespace' is not going to happen during this execution"
+            else
+              printInfo "All the resoures in the '$namespace' namespace are going to be idled"
+              oc idle -n $namespace --all
+            fi
+          else
+            printInfo "A workspace is running in the '$namespace-che'. Idling in the '$namespace' is not going to happen during this run"
+          fi
+        else
+          printInfo "Skipping '$namespace' namespace since it is not Hosted Che specific (there is no corresponding '$namespace-che' namespace for it)"
+        fi
+      fi
+  done
+}
+runIdler
